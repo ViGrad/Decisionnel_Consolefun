920f88ef28d834bfc784037321969377611dafb1
Vincent G <vincent.grandiere@gmail.com>
2018-11-12 09:38:46 +0100

    shiny project

---
+++ test.R
@@ -0,0 +1,77 @@
+library(shiny)
+
+# Contenu de l'interface
+ui <- fluidPage(
+  titlePanel("Variable qualitative"),
+  
+  sidebarLayout(
+      sidebarPanel(
+        column(10, 
+               # Bouton de recherche du fichier à charger
+               fileInput(inputId = "file1", label = "Choose TSV File",
+                         accept = c("text/plain", ".tsv")
+               ))
+      ),
+      mainPanel(
+      tabsetPanel(
+        tabPanel("Histogramme nouveaux utilisateurs",  fluidRow(
+          column(5, align="center",
+                 # Buton de mise à jour de la liste rv
+                 plotOutput("hist")),
+          column(3, align="center",
+                 # Buton de mise à jour de la liste rv
+                 verbatimTextOutput(outputId = "summary"))
+          )
+        ),
+        tabPanel("Histogramme utilisateurs par jour",  fluidRow(
+          column(5, align="center",
+                 # Buton de mise à jour de la liste rv
+                 plotOutput("histUsers")),
+          column(3, align="center",
+                 # Buton de mise à jour de la liste rv
+                 verbatimTextOutput(outputId = "summaryUsers"))
+        )
+        )
+    )
+  )
+  )
+)
+
+
+  # Commandes à exécuter
+  server <- function(input, output){
+    data <- read.csv("./output Nouveaux utilisateurs.csv", header = TRUE)
+    data2 <- read.csv("./output utilisateurs actifs.csv", header = TRUE)
+    
+    # Récupération des valeurs fecondite
+    nouveaux <- reactive({
+      if(!"nouveaux" %in% colnames(data())) return(NULL)
+      data()$nouveaux
+    })
+    
+    users <- reactive({
+      if(!"actifs" %in% colnames(data2())) return(NULL)
+      as.numeric((unlist(data2())))
+    })
+    
+    # On initialise liste de valeurs réactives
+    # ----
+    rv <- reactiveValues(hist_isFreq = TRUE, 
+                         hist_yLabel = "Effectifs", 
+                         hist_col = "blue")
+  
+    # Histogramme
+    # ----
+    output$hist <- renderPlot({
+      hist(nouveaux(), freq = rv$hist_isFreq)
+    })
+    
+    output$histUsers <- renderPlot({
+      hist(users(), freq = rv$hist_isFreq)
+    })
+    
+    output$summary <- renderPrint({ t(summary(data())) })
+    output$summaryUsers <- renderPrint({ t(summary(data2())) })
+  }
+  # Association interface & commandes
+  shinyApp(ui = ui, server = server)
