38b6b30b1a267444710314925387b610b490c6c0
Vincent G <vincent.grandiere@gmail.com>
2018-11-18 21:14:48 +0100

    p

--- shiny-project/src/categoryAnalysis.R
+++ shiny-project/src/categoryAnalysis.R
@@ -28,7 +28,7 @@ categoryAnalysis$ui <- fluidPage(
   , style = "font-size: 75%"
 )
 categoryAnalysis$server <- function(input, output){
-  data <- fread("./inputs/ensemble bd analytics.csv", header=TRUE)
+  data <- read.csv("./inputs/ensemble bd analytics.csv", header=TRUE)
   colnames(data) = c("Pages de destinations","Sessions","% nouvelles sessions", "Nouveaux utilisateurs", "Taux de rebond", "Pages/session", "Durée moyenne des sessions", "Visite (Taux de conversion – Objectif 1)", "Visite (Réalisations de l'objectif 1)", "Visite (valeur de l'objectif 1)", "idfichejeux", "titrejeux", "descriptif", "editeur", "developpeur", "type", "sortie", "sortie_jp", "sortie_us", "support", "icones", "classification","multijoueur","console","image","idforum","pub","videotest","tournoi","auteur","galerie","id","date","categorie","multi","titre","contenu","jouabilite","note_jouabilite","graphismes","note_graphismes","bandeson","note_bandeson","dureedevie","note_dureedevie","scenario","note_scenario","conclusion","note","plus","moins","selection","image1","image2","image3","lien","topic","anecdotes","etat","une","dossier","fiche", "source") 
   
   # Données brutes
@@ -39,45 +39,47 @@ categoryAnalysis$server <- function(input, output){
   # ----
   # Unidimensionnel
   output$barplotUni <- renderPlot({
-    # Diagramme en barres de la variable 'Level' avec ggplot
-    ggplot(data, aes(x = Level)) + geom_bar()
+    # Diagramme en barres de la variable 'categorie' avec ggplot
+    ggplot(data, aes(x = categorie)) + geom_bar()
   })
   output$barplotOrderedUni <- renderPlot({
-    # Diagramme en barres de la variable 'Level' avec ggplot
+    # Diagramme en barres de la variable 'categorie' avec ggplot
     tmp.data <- data
-    # On ordonne dans l'ordre naturel les différentes modalités de 'Level'
+    # On ordonne dans l'ordre naturel les différentes modalités de 'categorie'
     # freshman: 1, sophomore: 2, junior: 3, senior: 4, special: 5, graduate: 6
     tmp.order <- rep(0, nrow(data))
-    tmp.order[with(tmp.data, Level == "freshman")] = 1
-    tmp.order[with(tmp.data, Level == "sophomore")] = 2
-    tmp.order[with(tmp.data, Level == "junior")] = 3
-    tmp.order[with(tmp.data, Level == "senior")] = 4
-    tmp.order[with(tmp.data, Level == "special")] = 5
-    tmp.order[with(tmp.data, Level == "graduate")] = 6
-    tmp.data$Level <- with(tmp.data, reorder(Level, tmp.order))
+    tmp.order[with(tmp.data, categorie == "ps4")] = 1
+    tmp.order[with(tmp.data, categorie == "xboxone")] = 2
+    tmp.order[with(tmp.data, categorie == "switch")] = 3
+    tmp.order[with(tmp.data, categorie == "pc")] = 4
+    tmp.order[with(tmp.data, categorie == "iphone")] = 5
+    tmp.order[with(tmp.data, categorie == "android")] = 6
+    tmp.order[with(tmp.data, categorie == "multi")] = 4
+    
+    tmp.data$categorie <- with(tmp.data, reorder(categorie, tmp.order))
     rm(tmp.order)
-    # Diagramme en barres de la variable 'Level' avec ggplot
-    ggplot(tmp.data, aes(x = Level)) + geom_bar()
+    # Diagramme en barres de la variable 'categorie' avec ggplot
+    ggplot(tmp.data, aes(x = categorie)) + geom_bar()
   })
   
   # Bidimensionnel
   output$barplotBi <- renderPlot({
-    # Diagramme en barres entre les variables 'Level' et 'Sex'
-    ggplot(data, aes(x = Level, fill = Sex)) + geom_bar()
+    # Diagramme en barres entre les variables 'categorie' et 'Sessions'
+    ggplot(data, aes(x = categorie, fill = Sessions)) + geom_bar()
   })
   output$barplotProfils <- renderPlot({
-    # Diagramme de profils entre les variables 'Level' et 'Sex'
-    ggplot(data, aes(x = Level, fill = Sex)) + geom_bar(position = "fill")
+    # Diagramme de profils entre les variables 'categorie' et 'Sessions'
+    ggplot(data, aes(x = categorie, fill = Sessions)) + geom_bar(position = "fill")
   })
   output$barplotDodgeBi <- renderPlot({
-    # Diagramme de profils entre les variables 'Level' et 'Sex'
-    ggplot(data, aes(x = Level, fill = Sex)) + geom_bar(position = "dodge")
+    # Diagramme de profils entre les variables 'categorie' et 'Sessions'
+    ggplot(data, aes(x = categorie, fill = Sessions)) + geom_bar(position = "dodge")
   })
   
-  # Table de contingence entre 'Sex' et 'Level'
+  # Table de contingence entre 'Sessions' et 'categorie'
   # ----
   output$contingency <- renderTable({
-    tab = with(data, table(Sex, Level))
+    tab = with(data, table(Sessions, categorie))
     round(tab/sum(tab), 3)
     tab
   })
