9bb2d8cee36c67a6beddbf136d9a0cc16152a1d6
Vincent G <vincent.grandiere@gmail.com>
2018-11-19 09:17:35 +0100

    update

--- shiny-project/src/categoryAnalysis.R
+++ shiny-project/src/categoryAnalysis.R
@@ -7,19 +7,19 @@ categoryAnalysis$ui <- fluidPage(
   tabsetPanel(
     tabPanel("Diag. Barres (1 var.)", 
              fluidRow(
-               column(6, plotOutput("barplotUni")),
-               column(6, plotOutput("barplotOrderedUni"))
+               column(12, plotOutput("barplotUni")),
+               column(12, plotOutput("barplotOrderedUni"))
              )
     ),
     tabPanel("Diag. Barres (2 var.)", 
              fluidRow(
-               column(6, plotOutput("barplotBi")),
-               column(6, plotOutput("barplotDodgeBi"))
+               column(12, plotOutput("barplotBi")),
+               column(12, plotOutput("barplotDodgeBi"))
              )
     ),
     tabPanel("Diag. Profils", 
              fluidRow(
-               column(6, plotOutput("barplotProfils")),
+               column(12, plotOutput("barplotProfils")),
                column(6, tableOutput("contingency"))
              )
     ),
@@ -37,47 +37,47 @@ categoryAnalysis$server <- function(input, output){
   # ----
   # Unidimensionnel
   output$barplotUni <- renderPlot({
-    # Diagramme en barres de la variable 'categorie' avec ggplot
-    ggplot(data, aes(x = categorie)) + geom_bar()
+    # Diagramme en barres de la variable 'genre' avec ggplot
+    ggplot(data, aes(x = genre)) + geom_bar()
   })
   output$barplotOrderedUni <- renderPlot({
-    # Diagramme en barres de la variable 'categorie' avec ggplot
+    # Diagramme en barres de la variable 'genre' avec ggplot
     tmp.data <- data
-    # On ordonne dans l'ordre naturel les différentes modalités de 'categorie'
+    # On ordonne dans l'ordre naturel les différentes modalités de 'genre'
     # freshman: 1, sophomore: 2, junior: 3, senior: 4, special: 5, graduate: 6
     tmp.order <- rep(0, nrow(data))
-    tmp.order[with(tmp.data, categorie == "ps4")] = 1
-    tmp.order[with(tmp.data, categorie == "xboxone")] = 2
-    tmp.order[with(tmp.data, categorie == "switch")] = 3
-    tmp.order[with(tmp.data, categorie == "pc")] = 4
-    tmp.order[with(tmp.data, categorie == "iphone")] = 5
-    tmp.order[with(tmp.data, categorie == "android")] = 6
-    tmp.order[with(tmp.data, categorie == "multi")] = 4
+    tmp.order[with(tmp.data, genre == "ps4")] = 1
+    tmp.order[with(tmp.data, genre == "xboxone")] = 2
+    tmp.order[with(tmp.data, genre == "switch")] = 3
+    tmp.order[with(tmp.data, genre == "pc")] = 4
+    tmp.order[with(tmp.data, genre == "iphone")] = 5
+    tmp.order[with(tmp.data, genre == "android")] = 6
+    tmp.order[with(tmp.data, genre == "multi")] = 4
     
-    tmp.data$categorie <- with(tmp.data, reorder(categorie, tmp.order))
+    tmp.data$genre <- with(tmp.data, reorder(genre, tmp.order))
     rm(tmp.order)
-    # Diagramme en barres de la variable 'categorie' avec ggplot
-    ggplot(tmp.data, aes(x = categorie)) + geom_bar()
+    # Diagramme en barres de la variable 'genre' avec ggplot
+    ggplot(tmp.data, aes(x = genre)) + geom_bar()
   })
   
   # Bidimensionnel
   output$barplotBi <- renderPlot({
-    # Diagramme en barres entre les variables 'categorie' et 'genre'
-    ggplot(data, aes(x = categorie, fill = genre)) + geom_bar()
+    # Diagramme en barres entre les variables 'genre' et 'categorie'
+    ggplot(data, aes(x = genre, fill = categorie)) + geom_bar()
   })
   output$barplotProfils <- renderPlot({
-    # Diagramme de profils entre les variables 'categorie' et 'genre'
-    ggplot(data, aes(x = categorie, fill = genre)) + geom_bar(position = "fill")
+    # Diagramme de profils entre les variables 'genre' et 'categorie'
+    ggplot(data, aes(x = genre, fill = categorie)) + geom_bar(position = "fill")
   })
   output$barplotDodgeBi <- renderPlot({
-    # Diagramme de profils entre les variables 'categorie' et 'genre'
-    ggplot(data, aes(x = categorie, fill = genre)) + geom_bar(position = "dodge")
+    # Diagramme de profils entre les variables 'genre' et 'categorie'
+    ggplot(data, aes(x = genre, fill = categorie)) + geom_bar(position = "dodge")
   })
   
-  # Table de contingence entre 'genre' et 'categorie'
+  # Table de contingence entre 'categorie' et 'genre'
   # ----
   output$contingency <- renderTable({
-    tab = with(data, table(genre, categorie))
+    tab = with(data, table(categorie, genre))
     round(tab/sum(tab), 3)
     tab
   })
